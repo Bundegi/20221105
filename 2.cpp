4326cd886980d971181669e3612c06e86fdd8ff2
Bundegi <106502120+Bundegi@users.noreply.github.com>
2022-11-05 14:30:06 +0900

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,7 +4,10 @@
 #define fast ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr)
 #define endl "\n"
 using namespace std;
-
+//1912backjun dp문제 연속합
+//아직 dp는 머리속에서 개념이 잘 안잡히는 느낌.
+//조금 더 dp를 파봐서 유형들을 머리 속에 넣어놔야 할 것 같음.
+//이 문제도 답을 본건 아니지만 힌트로 인해서 풀었음(그게답인가)
 int main() {
 	fast;
 	int n;
